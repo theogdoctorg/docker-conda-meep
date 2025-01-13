168b03679b89327504fb9471c8e5d47b0494eb52
theogdoctorg <garrisoncrouch@gmail.com>
2025-01-12 18:23:29 -0800

    adding creation of notebooks folder
    Signed-off-by: theogdoctorg <garrisoncrouch@gmail.com>

--- parallel_jupyter.sh
+++ parallel_jupyter.sh
@@ -2,4 +2,5 @@ conda init
 bash ~/.bashrc
 . ~/.bashrc
 conda activate pmp
+mkdir -p /home/host/notebooks
 jupyter notebook --notebook-dir=/home/host/notebooks --ip='*' --port=8888 --no-browser --allow-root
